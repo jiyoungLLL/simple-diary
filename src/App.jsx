df7977713469fe7757b6629abbffe911c506046c
hackurity01 <hackurity01@gmail.com>
2024-09-09 01:08:06 +0900

    메인 화면 기본 구성

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,22 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
-
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
+    <div
+      style={{
+        maxWidth: 400,
+        background: "#f7f7f7",
+        margin: "auto",
+        height: "100vh",
+      }}>
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
+        <div>Header</div>
+        <div>
+          <button>버튼</button>
+        </div>
+      </div>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <textarea style={{ width: "100%", boxSizing: "border-box" }} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   );
 }
 
