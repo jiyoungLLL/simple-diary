9ec027eec03e9e6e4eb9604d4b5b98e9108ccbf3
hackurity01 <hackurity01@gmail.com>
2024-09-09 02:02:36 +0900

    질문 api 연동

--- src/components/MainView.jsx
+++ src/components/MainView.jsx
@@ -1,5 +1,5 @@
 import "./MainView.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const diary = JSON.parse(window.localStorage.getItem("diary") || "{}");
 
@@ -10,8 +10,23 @@ function MainView(props) {
   const date = now.getDate();
 
   const [text, setText] = useState(diary[date]);
+  const [questions, setQuestions] = useState();
 
   const today = `${year}년 ${month}월 ${date}일`;
+
+  useEffect(() => {
+    fetch(
+      "https://raw.githubusercontent.com/hackurity01/simple-diary/main/src/questions.json"
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setQuestions(data);
+      });
+  }, []);
+
+  if (!questions) {
+    return "loading";
+  }
 
   return (
     <>
@@ -22,10 +37,11 @@ function MainView(props) {
             onClick={() => {
               props.setView("history");
             }}>
-            버튼
+            기록 보기
           </button>
         </div>
       </div>
+      <div className="question">{questions[date]}</div>
       <div className="content">
         <textarea
           value={text}
