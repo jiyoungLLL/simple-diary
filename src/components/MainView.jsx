4763d4dcddad3a32c02a24bdfd82d6d8e3a6f149
[AI10] 임지영 <dlawldud021@naver.com>
2024-11-18 21:13:39 +0900

    localStorage 저장

--- src/components/MainView.jsx
+++ src/components/MainView.jsx
@@ -5,6 +5,8 @@ import './MainView.css';
 function MainView({ setView }) {
   const [questions, setQuestions] = useState();
   const today = dayjs();
+  const answers = JSON.parse(localStorage.getItem('diary') || {});
+  const [input, setInput] = useState(answers[today.get('D')]);
 
   useEffect(() => {
     fetch(
@@ -42,8 +44,16 @@ function MainView({ setView }) {
       <div className='question'>{questions[`${today.get('D')}`]}</div>
       <div className='content'>
         <textarea
-          onChange={() => {
+          value={input}
+          onChange={(e) => {
+            const value = e.target.value;
             console.log('onChange');
+            setInput(value);
+            localStorage.setItem(
+              'diary',
+              // ..answers를 추가함으로써 다른 날짜의 내용도 유지함
+              JSON.stringify({ ...answers, [today.get('D')]: value })
+            );
           }}
         />
       </div>
